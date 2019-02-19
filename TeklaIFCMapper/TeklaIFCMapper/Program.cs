6cd0ed01007e528f3623cb6302ce6583b44b53e0
loaialaddien <41028136+loaialaddien@users.noreply.github.com>
2019-02-19 18:48:59 +0200

    Static ReadFromRevit

--- TeklaIFCMapper/TeklaIFCMapper/Program.cs
+++ TeklaIFCMapper/TeklaIFCMapper/Program.cs
@@ -24,29 +24,36 @@ namespace TeklaIFCMapper
 
                
             }
-            using (var model = IfcStore.Open(filepath))
+            using (var model = IfcStore.Open("Nignog.ifc"))
             {
 
 
-                string section = "PL150*5";
-                string section2 = "PL200*5";
-                string section3 = "PL180*5";
 
-                ModelInfo mymodel = new ModelInfo("project1", "site1", "building1", filepath, model);
-                TeklaElement myelement = new TeklaElement(section, mymodel);
-                myelement.Create(model);
-                TeklaElement myelement2 = new TeklaElement(section2, mymodel);
-                myelement2.Create(model);
+                //string section = "PL150*5";
+                //string section2 = "PL200*5";
+                //string section3 = "PL180*5";
 
-                TeklaElement myelement3 = new TeklaElement(section3, mymodel);
-                myelement3.Create(model);
+                //ModelInfo mymodel = new ModelInfo("project1", "site1", "building1", filepath, model);
+                //TeklaElement myelement = new TeklaElement(section, mymodel);
+                //myelement.Create(model);
+                //TeklaElement myelement2 = new TeklaElement(section2, mymodel);
+                //myelement2.Create(model);
 
-                ElementAssembly elementAssembly = new ElementAssembly(mymodel);
-                elementAssembly.MyElements.Add(myelement);
-                elementAssembly.MyElements.Add(myelement2);
-                elementAssembly.MyElements.Add(myelement3);
+                //TeklaElement myelement3 = new TeklaElement(section3, mymodel);
+                //myelement3.Create(model);
 
-                elementAssembly.Create(model);
+                //ElementAssembly elementAssembly = new ElementAssembly(mymodel);
+                //elementAssembly.MyElements.Add(myelement);
+                //elementAssembly.MyElements.Add(myelement2);
+                //elementAssembly.MyElements.Add(myelement3);
+
+                //elementAssembly.Create(model);
+                var list = RevitToTeklaIfc.RetrieveColumns(model);
+                var col = list[0];
+                double w; double l; double c; double v; double r;
+                RevitToTeklaIfc.RetrieveLenghtWidthHeight(col,out w,out l,out c,out v,out r);
+
+                Console.Read();
             }
 
                 string filename = "D:\\iti\\courses\\BIM\\bim I project\\TeklaIFCMapper\\TeklaIFCMapper\\TeklaIFCMapper\\bin\\Debug";
---
+++ TeklaIFCMapper/TeklaIFCMapper/XbimWrapper/RevitToTeklaIfc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.Ifc;
+using Xbim.Ifc2x3.GeometricConstraintResource;
+using Xbim.Ifc2x3.GeometricModelResource;
+using Xbim.Ifc2x3.GeometryResource;
+using Xbim.Ifc2x3.Kernel;
+using Xbim.Ifc2x3.ProfileResource;
+using Xbim.Ifc2x3.RepresentationResource;
+using Xbim.Ifc2x3.SharedBldgElements;
+
+namespace TeklaIFCMapper.XbimWrapper
+{
+    class RevitToTeklaIfc
+    {
+        public static List<IfcColumn> RetrieveColumns(IfcStore model)
+        {
+            List<IfcColumn> colList = new List<IfcColumn>();
+            var list = model.Instances.OfType<IfcColumn>();
+            colList = list.ToList();
+            return colList;
+
+        }
+        public static IfcObjectPlacement RetrieveLocalPlacement(IfcStore model, IfcColumn column, out List<IfcLocalPlacement> ifcLocal, out IfcCartesianPoint cartesianPoint)
+        {
+            var objectplacement = column.ObjectPlacement;
+            ifcLocal = objectplacement.ReferencedByPlacements.ToList();
+            cartesianPoint = ((IfcAxis2Placement3D)ifcLocal.FirstOrDefault().RelativePlacement).Location;
+            
+            return objectplacement;
+            
+        }
+        public static void RetrieveLenghtWidthHeight(IfcColumn column, out double width, out double depth , out double Flangethickness, out double webthickness, out double filletRadius)
+        {
+           
+         width=   ((IfcIShapeProfileDef)column.Representation.Representations.OfType<IfcShapeRepresentation>().FirstOrDefault().Items.OfType<IfcMappedItem>().FirstOrDefault().MappingSource.MappedRepresentation.Items.OfType<IfcExtrudedAreaSolid>().FirstOrDefault().SweptArea).OverallWidth;
+            depth = ((IfcIShapeProfileDef)column.Representation.Representations.OfType<IfcShapeRepresentation>().FirstOrDefault().Items.OfType<IfcMappedItem>().FirstOrDefault().MappingSource.MappedRepresentation.Items.OfType<IfcExtrudedAreaSolid>().FirstOrDefault().SweptArea).OverallDepth;
+            webthickness  = ((IfcIShapeProfileDef)column.Representation.Representations.OfType<IfcShapeRepresentation>().FirstOrDefault().Items.OfType<IfcMappedItem>().FirstOrDefault().MappingSource.MappedRepresentation.Items.OfType<IfcExtrudedAreaSolid>().FirstOrDefault().SweptArea).WebThickness;
+            Flangethickness = ((IfcIShapeProfileDef)column.Representation.Representations.OfType<IfcShapeRepresentation>().FirstOrDefault().Items.OfType<IfcMappedItem>().FirstOrDefault().MappingSource.MappedRepresentation.Items.OfType<IfcExtrudedAreaSolid>().FirstOrDefault().SweptArea).FlangeThickness;
+            filletRadius =(double) ((IfcIShapeProfileDef)column.Representation.Representations.OfType<IfcShapeRepresentation>().FirstOrDefault().Items.OfType<IfcMappedItem>().FirstOrDefault().MappingSource.MappedRepresentation.Items.OfType<IfcExtrudedAreaSolid>().FirstOrDefault().SweptArea).FilletRadius;
+
+            
+
+        }
+
+    }
+}
